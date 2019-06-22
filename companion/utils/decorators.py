0398a2808e8d7a669b13f052e84111071452ca18
nitanmarcel <nitan.marcel@protonmail.com>
2019-06-22 22:04:11 +0300

    Fix admin check decorator

--- companion/utils/decorators.py
+++ companion/utils/decorators.py
@@ -103,14 +103,12 @@ def admins_only(f):
     async def wrapper(event):
         if not event.is_private:
             chat = await event.get_chat()
-            if not chat.creator or not chat.admin_rights:
-                await event.reply("This command was made for chat admins only!")
-                return
-            else:
+            if chat.creator or chat.admin_rights:
                 chat_creator = chat.creator
                 chat_admin_rights = chat.admin_rights
-
-        return await f(event, chat, chat_creator, chat_admin_rights)
+                return await f(event, chat, chat_creator, chat_admin_rights)
+            else:
+                await event.reply("This command was made for chat admins only!")
     return wrapper
 
 
