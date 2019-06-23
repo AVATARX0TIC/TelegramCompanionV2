f0f1643e8596848fd41e200b9dec3429231d76f6
nitanmarcel <nitan.marcel@protonmail.com>
2019-06-23 17:48:54 +0300

    Return all admin rights if a command is used in private

--- companion/utils/decorators/chat_status.py
+++ companion/utils/decorators/chat_status.py
@@ -11,4 +11,20 @@ def admins_only(f):
                 return await f(event, chat, chat_creator, chat_admin_rights)
             else:
                 await event.reply("This command was made for chat admins only!")
+        else:
+            chat = await event.get_chat()
+            chat_creator = True
+            chat_admin_rights = PrivateAdminRights()
+            return await f(event, chat, chat_creator, chat_admin_rights)
     return wrapper
+
+class PrivateAdminRights:
+    def __init__(self):
+        self.change_info = True
+        self.post_messages = True
+        self.edit_messages = True
+        self.delete_messages = True
+        self.ban_users = True
+        self.invite_users = True
+        self.pin_messages = True
+        self.add_admins = True
