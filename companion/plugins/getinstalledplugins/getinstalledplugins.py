488e8a4e3ed4c5b90f7c3b6791a7766afec0e565
nitanmarcel <nitan.marcel@protonmail.com>
2019-06-26 22:33:23 +0300

    Fix the plugin getter commands

--- companion/plugins/getinstalledplugins/getinstalledplugins.py
+++ companion/plugins/getinstalledplugins/getinstalledplugins.py
@@ -12,9 +12,8 @@ async def get_plugin_info(event):
     if not plugin:
         await event.edit("Give me a plugin name or use the `plugins` command to list all the available plugins!")
     else:
-        info = await load_plugin_info(plugin)
+        info = load_plugin_info(plugin)
         if info:
-            plugin = plugin
             await event.edit("Plugin info for: " + plugin + ":" +
                              "\nName: " + info.get("main", "Not Available!") +
                              "\nAuthor: " + info.get("author", "Not Available!") +
